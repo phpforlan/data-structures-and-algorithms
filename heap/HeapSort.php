aa757e341964fbc208dd6104fe26369da0a43a7d
lurenzhong <lurenzhong@xiaomi.com>
2017-03-07 11:33:13 +0800

    堆排序重写

--- heap/HeapSort.php
+++ heap/HeapSort.php
@@ -2,34 +2,33 @@
 /**
  * @file HeapSort.php
  * @author lurenzhong@xiaomi.com
- * @brief php实现堆排序
+ * @brief 堆排序
  */
 
-//引入初始化最大堆类
 require_once './InitHeap.php';
 
 function heapSort(&$arr)
 {
     $len = count($arr);
 
-    /*
-      每次把堆顶(最大值)交换到堆尾,然后对去除堆尾的剩余元素初始化最大堆。重复该步骤
-      这样每次最大值就放在堆尾了，完成了排序
-     */
-    for($i = $len; $i>0; $i--){ //$i是数组要初始化最大堆的长度
+    if( $len <= 1 ){
+        return $arr;
+    }
 
-        $heapObj = new Heap();
-        $heapObj->init($arr, $i);
+    $initHeapObj = new InitHeap();
 
-        //交换堆顶与堆尾结点
-        $tmp = $arr[$i-1];
-        $arr[$i-1] = $arr[0];
+    for($i=0; $i<$len; $i++){
+        $initHeapObj->init($arr, $len-$i);
+
+        //将堆顶与堆尾元素进行交换
+        $tmp = $arr[$len-$i-1];
+        $arr[$len-$i-1] = $arr[0];
         $arr[0] = $tmp;
     }
+
 }
 
-
-$arr = [1,8,3,6,0,2,7,4,5];
-/** 测试堆排序 **/
+$arr = [10,23,45,5,22,9,123];
 heapSort($arr);
+
 print_r($arr);
