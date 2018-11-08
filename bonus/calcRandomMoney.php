c7cd8cf131a6250df621f28ed670df078c46e4a4
lurenzhong <lurenzhong@didichuxing.com>
2018-11-08 11:37:10 +0800

    fix

--- bonus/calcRandomMoney.php
+++ bonus/calcRandomMoney.php
@@ -7,12 +7,40 @@
  * @version
  */
 
-class bonus {
+class Bonus
+{
 
     private $total; //总金额
     private $min; //单个红包最小金额
     private $max; //单个红包最大金额
     private $num; //红包数量
 
+    public function randomMoney()
+    {
+        $average = $this->total / $this->num;
+
+        if (mt_rand($this->min, $this->max) > $average) { //如果大于平均金额
+
+        }
+    }
+
+
+    /**
+     * 计算$bonusMin到$bonusMax之间的随机数
+     * @param $bonusMin
+     * @param $bonusMax
+     * @return int
+     */
+    public function xRandom($bonusMin, $bonusMax)
+    {
+        $sqr = pow($bonusMax - $bonusMin, 2);
+        $randNum = mt_rand(0, $sqr - 1);
+
+        return intval(sqrt($randNum));
+    }
+
 
 }
+
+$bonus = new Bonus();
+echo $bonus->xRandom(10, 20);
