de7ea615e22ae0f5615d9a435187e6497fe38c23
lurenzhong <lurenzhong@didichuxing.com>
2019-01-27 19:42:43 +0800

    使用java中的数组

--- src/main/java/com/imooc/data/structure/main.java
+++ src/main/java/com/imooc/data/structure/main.java
@@ -4,7 +4,25 @@ public class main {
 
     public static void main(String[] args) {
 
-        System.out.println(1);
+        int[] arr = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+
+        for (int item : arr) { //foreach
+            //System.out.println(item);
+        }
+
+        int[] scores = new int[]{100, 99, 98};
+        for (int i = 0; i < scores.length; i++) {
+            //System.out.println(scores[i]);
+        }
+
+        scores[0] = 98;
+        for (int score : scores) {
+            System.out.println(score);
+        }
 
     }
 }
