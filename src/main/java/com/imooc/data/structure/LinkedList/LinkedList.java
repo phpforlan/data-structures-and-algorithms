2c37718af22091d80a2ff9010bc9a58b89b404ff
lurenzhong <lurenzhong@didichuxing.com>
2019-03-06 11:32:58 +0800

    单向链表实现

--- src/main/java/com/imooc/data/structure/LinkedList/LinkedList.java
+++ src/main/java/com/imooc/data/structure/LinkedList/LinkedList.java
@@ -1,11 +1,14 @@
 package com.imooc.data.structure.LinkedList;
 
 /**
- * 单向链表LinkedList实现
+ * 单向链表实现(无虚拟头节点dummyHead)
+ *
+ * @param <E>
  */
 public class LinkedList<E> {
 
-    //Node节点定义
+
+    //节点Node定义
     private class Node {
 
         public E e;
@@ -31,17 +34,14 @@ public class LinkedList<E> {
         }
     }
 
-    private Node head = null;
+    private Node head; //头节点
+    private Integer size; //链表中节点的数量
 
-    private Integer size = 0; //链表中node节点的数量
 
-
-    //获取链表中节点的个数
-    public int getSize() {
+    public Integer getSize() {
         return size;
     }
 
-    //链表是否为空
     public boolean isEmpty() {
         return size == 0;
     }
@@ -54,33 +54,32 @@ public class LinkedList<E> {
     public void addFirst(E e) {
 
         Node node = new Node(e);
+
         node.next = head;
         head = node;
-
-        //等同于
-        //head = new Node(e, head);
 
         size++;
     }
 
+
     /**
-     * 在链表指定索引index(0-based)位置插入节点
+     * 链表指定索引(0-based)位置添加节点
      *
      * @param index
      * @param e
      */
     public void add(Integer index, E e) {
 
-        //校验index合法性
+        //校验索引是否合法
         if (index < 0 || index > size) {
-            throw new IllegalArgumentException("add failed, Illegal index.");
+            throw new IllegalArgumentException("Illegal index.");
         }
 
-        if (index == 0) { //要插入位置没有前置节点prev
+        if (index == 0) {
             addFirst(e);
         } else {
 
-            Node prev = head; //index的前一个元素
+            Node prev = head; //要插入索引的前一个位置
             for (int i = 0; i < index - 1; i++) {
                 prev = prev.next;
             }
@@ -94,51 +93,109 @@ public class LinkedList<E> {
     }
 
     /**
-     * 链表尾部添加节点
+     * 在链表尾部添加节点
      *
      * @param e
      */
     public void addLast(E e) {
+
         add(size, e);
     }
 
-    @Override
-    public String toString() {
 
-        StringBuilder res = new StringBuilder();
-        res.append("LinkedList head [");
+    /**
+     * 获取链表指定索引节点的数据
+     *
+     * @param index
+     * @return
+     */
+    public E get(Integer index) {
 
-        Node node = head;
-        for (int i = 0; i < size; i++) {
-
-            res.append(node);
-
-            node = node.next;
-
-            if (i != size - 1) { //非最后一个节点
-                res.append(", ");
-            }
+        //校验索引
+        if (index < 0 || index > size - 1) {
+            throw new IllegalArgumentException("Illegal index.");
         }
 
-        res.append("] tail");
+        Node cur = head;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
 
-        return res.toString();
+        return cur.e;
+    }
+
+    /**
+     * 获取链表头部节点数据
+     *
+     * @return
+     */
+    public E getFirst() {
+
+        return get(0);
     }
 
 
-    public static void main(String[] args) {
+    /**
+     * 修改指定索引节点的数据
+     *
+     * @param index
+     * @param e
+     */
+    public void set(Integer index, E e) {
 
-        LinkedList<Integer> linkedList = new LinkedList<>();
-
-        for (int i = 0; i < 5; i++) {
-            linkedList.add(0, i);
+        //校验索引
+        if (index < 0 || index > size - 1) {
+            throw new IllegalArgumentException("Illegal index.");
         }
 
-        linkedList.add(1, 66);
-        linkedList.addLast(100);
+        Node cur = head;
+        for (int i = 0; i < index; i++) {
+            cur = cur.next;
+        }
 
-        System.out.println(linkedList);
+        cur.e = e;
     }
 
+
+    /**
+     * 删除头结点
+     *
+     * @return
+     */
+    public E removeFirst() {
+
+        Node tmp = head.next;
+        Node ret = head;
+        head = null;
+        head = tmp;
+
+        return ret.e;
+    }
+
+    /**
+     * 删除指定索引(0-based)的节点
+     *
+     * @param index
+     * @return
+     */
+    public E remove(Integer index) {
+
+        //校验索引
+        if (index < 0 || index > size - 1) {
+            throw new IllegalArgumentException("Illegal index.");
+        }
+
+        Node prev = head;
+        for (int i = 0; i < index - 1; i++) {
+            prev = prev.next;
+        }
+
+        Node tmp = prev.next.next;
+        Node ret = prev.next;
+        prev.next = null;
+        prev.next = tmp;
+
+        return ret.e;
+    }
 
 }
