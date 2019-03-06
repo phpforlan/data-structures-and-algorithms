5f2fadd35632039820d709fc61921fb928e24070
lurenzhong <lurenzhong@didichuxing.com>
2019-03-06 21:23:27 +0800

    解决leetcode求两数之和问题

--- src/main/java/com/imooc/data/structure/LinkedList/AddTwoNumbers.java
+++ src/main/java/com/imooc/data/structure/LinkedList/AddTwoNumbers.java
@@ -1,6 +1,8 @@
 package com.imooc.data.structure.LinkedList;
 
+import javax.xml.soap.Node;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 2.两数相加
@@ -15,21 +17,24 @@ import java.util.LinkedList;
  * 原因：342 + 465 = 807
  * <p>
  */
-public class AddTwoNumbers {
 
-    //节点定义
-    private class ListNode {
-        int val;
-        ListNode next;
+//节点定义
+class ListNode {
+    int val;
+    ListNode next;
 
-        ListNode(int x) {
-            val = x;
-        }
+    ListNode(int x) {
+        val = x;
     }
+}
+
+public class AddTwoNumbers {
 
     /**
      * 两数之和
      * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4) 逆序存储
+     * <p>
+     * l1.val+l2.val+carry
      *
      * @param l1
      * @param l2
@@ -37,33 +42,71 @@ public class AddTwoNumbers {
      */
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
+        ListNode dummyHead = new ListNode(0); //虚拟头结点
+        int carry = 0; //商
+
         ListNode cur1 = l1;
         ListNode cur2 = l2;
 
-        ListNode cur = new ListNode(0);
+        ListNode cur = dummyHead;
+        while (cur1 != null || cur2 != null) { //核心公式: l1.val+l2.val+carry
 
-        while (cur1 != null) {
+            int x = cur1 == null ? 0 : cur1.val;
+            int y = cur2 == null ? 0 : cur2.val;
 
-            while (cur2 != null) { //l1和l2当前节点值都不为空
+            int sum = x + y + carry;
 
-                int sum = cur1.val + cur2.val;
-                cur.val = cur.val + sum % 10;
+            //计算节点值
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
 
-                cur.next = new ListNode(sum / 10);
+            //计算带入下一次计算的商
+            carry = sum / 10;
 
-                cur = cur.next;
-
+            if (cur1 != null) {
+                cur1 = cur1.next;
+            }
+            if (cur2 != null) {
                 cur2 = cur2.next;
             }
-
-            cur1 = cur1.next;
         }
 
-        return cur;
+        if (carry == 1) { //说明还有进位
+            cur.next = new ListNode(carry);
+        }
+
+        return dummyHead.next;
     }
 
 
     public static void main(String[] args) {
+
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
+        ListNode head = addTwoNumbers.addTwoNumbers(l1, l2);
+
+        //遍历
+        StringBuilder res = new StringBuilder();
+        res.append("LinkedList: ");
+
+        ListNode cur = head;
+        while (cur != null) {
+
+            res.append(cur.val + "->");
+
+            cur = cur.next;
+        }
+
+        res.append("NULL");
+
+        System.out.println(res.toString());
 
     }
 }
