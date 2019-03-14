43e1b862f0785bd05a25ff36cf24a543c502a9b2
lurenzhong <lurenzhong@didichuxing.com>
2019-03-14 11:35:35 +0800

    循环队列自动扩容

--- src/main/java/com/imooc/data/structure/queue/LoopQueue.java
+++ src/main/java/com/imooc/data/structure/queue/LoopQueue.java
@@ -1,21 +1,23 @@
 package com.imooc.data.structure.queue;
 
-import com.imooc.data.structure.array.Array;
-
 /**
- * 循环队列(front=tail，则表示队列为空。(tail+1)%c=front，则表示队列满了)
- * <p>
- * tail表示下一个要入队元素存放的位置索引
+ * 通过静态数组实现循环队列(队首出，队尾进)
  *
  * @param <E>
  */
 public class LoopQueue<E> implements Queue<E> {
 
     private E[] data;
-
-    private int front, tail;
-
+    private int front;
+    private int tail; //下一个入队元素放置的位置
     private int size;
+
+    public LoopQueue() {
+        data = (E[]) new Object[11];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
 
     public LoopQueue(int capacity) {
         data = (E[]) new Object[capacity + 1];
@@ -24,27 +26,33 @@ public class LoopQueue<E> implements Queue<E> {
         size = 0;
     }
 
-    public LoopQueue() {
-        this(10);
-    }
-
-    public int getCapacity() { //队列设计会浪费一个空间
-        return data.length - 1;
-    }
-
 
     @Override
     public int getSize() {
         return size;
     }
 
+    /**
+     * 获取队列的容量
+     *
+     * @return
+     */
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    /**
+     * 队首和队尾指针指向同一个位置时，队列为空
+     *
+     * @return
+     */
     @Override
     public boolean isEmpty() {
         return front == tail;
     }
 
     /**
-     * 入队
+     * 元素队尾入队
      *
      * @param e
      */
@@ -52,8 +60,10 @@ public class LoopQueue<E> implements Queue<E> {
     public void enqueue(E e) {
 
         //校验队列是否已满
-        if ((tail + 1) % data.length == front) {
-            resize(getCapacity() * 2); //扩容X2
+        if ((tail + 1) % data.length == front) { //有意浪费一个空间
+
+            //自动扩容
+            resize(getCapacity() * 2);
         }
 
         data[tail] = e;
@@ -61,42 +71,62 @@ public class LoopQueue<E> implements Queue<E> {
         size++;
     }
 
-
     /**
-     * 出队
+     * 自动扩容
      *
-     * @return
+     * @param newCapacity
      */
+    public void resize(int newCapacity) {
+
+        E[] newData = (E[]) new Object[newCapacity + 1];
+
+        int j = 0;
+        for (int i = front; i != tail; i = (i + 1) % data.length) {
+            newData[j] = data[i];
+            j++;
+        }
+
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
     @Override
     public E dequeue() {
 
         //校验队列是否为空
         if (isEmpty()) {
-            throw new IllegalArgumentException("queue is empty");
+            throw new IllegalArgumentException("queue is empty.");
         }
 
-        E ret = data[front];
-
+        E e = data[front];
         data[front] = null;
-        front = (front + 1) % data.length; //front向前移动一位
+        front = (front + 1) % data.length;
+
         size--;
 
-        return ret;
+        return e;
     }
 
+    @Override
+    public E getFront() {
+        return data[front];
+    }
 
     @Override
     public String toString() {
 
         StringBuilder res = new StringBuilder();
+        res.append("queue front: [");
 
-        res.append("queue front [");
+        int cur = front;
+        for (int i = 0; i < size; i++) {
+            res.append(data[cur]);
 
-        for (int i = front; i != tail; i = (i + 1) % data.length) {
-            res.append(data[i]);
+            cur = (cur + 1) % data.length;
 
-            if ((i + 1) % data.length != tail) { //队尾元素
-                res.append(", ");
+            if (i != size - 1) {
+                res.append(",");
             }
         }
 
@@ -106,57 +136,15 @@ public class LoopQueue<E> implements Queue<E> {
     }
 
 
-    /**
-     * 获取队首元素
-     *
-     * @return
-     */
-    @Override
-    public E getFront() {
-        return data[front];
-    }
-
-
-    /**
-     * 队列扩容
-     *
-     * @param newCapacity 新容量
-     */
-    private void resize(int newCapacity) {
-
-        E[] newData = (E[]) new Object[newCapacity + 1];
-
-        for (int i = 0; i < size; i++) { //从索引0开始填充新data
-
-            newData[i] = data[(i + front) % data.length]; //从i开始偏移front个位置
-        }
-
-        data = newData;
-        front = 0;
-        tail = size;
-    }
-
     public static void main(String[] args) {
 
         LoopQueue<Integer> loopQueue = new LoopQueue<>();
 
-        //入队
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 15; i++) {
             loopQueue.enqueue(i);
             System.out.println(loopQueue);
         }
 
-        System.out.println("start dequeue...");
-
-        //出队
-        for (int i = 0; i < 5; i++) {
-            loopQueue.dequeue();
-            System.out.println(loopQueue);
-        }
-
-        System.out.println(loopQueue.front);
-        System.out.println(loopQueue.tail);
-        System.out.println(loopQueue.size);
-
     }
+
 }
