cd3403f136198a72eb01db903b75b08921b31040
lurenzhong <lurenzhong@didichuxing.com>
2019-03-21 11:17:24 +0800

    冒泡排序

--- src/main/java/com/imooc/data/structure/sort/BubbleSort.java
+++ src/main/java/com/imooc/data/structure/sort/BubbleSort.java
@@ -33,13 +33,44 @@ public class BubbleSort {
         return nums;
     }
 
+
+    /**
+     * 从大到小排序
+     *
+     * @param nums
+     * @return
+     */
+    public int[] sort2(int[] nums) {
+
+        int len = nums.length;
+
+        for (int i = 0; i < len - 1; i++) { //比较轮数
+
+            for (int j = 0; j < len - i - 1; j++) { //比较次数
+
+                if (nums[j] < nums[j + 1]) {
+                    int tmp = nums[j + 1];
+                    nums[j + 1] = nums[j];
+                    nums[j] = tmp;
+                }
+            }
+        }
+
+        return nums;
+    }
+
+
     public static void main(String[] args) {
 
         int[] nums = new int[]{10, 18, 12, 6, 13};
 
+        //从小到大排序
         BubbleSort bubbleSort = new BubbleSort();
         int[] sortedNums = bubbleSort.sort(nums);
+        System.out.println(Arrays.toString(sortedNums));
 
+        //从大到小排序
+        int[] sortNums2 = bubbleSort.sort2(nums);
         System.out.println(Arrays.toString(sortedNums));
 
     }
