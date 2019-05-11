4c3aa16023e1243c85bc6976585252c8822a4b03
lurenzhong <lurenzhong@didichuxing.com>
2019-05-11 18:00:39 +0800

    fix

--- php/buy/CalBuy.php
+++ php/buy/CalBuy.php
@@ -103,8 +103,8 @@ class CalBuy
      */
     public function test()
     {
-        $currentAveragePrice = 84.88;
-        $currentNum = 16;
+        $currentAveragePrice = 84.88; //当前仓位均价
+        $currentNum = 16; //当前仓位张数
 
         $this->run($currentAveragePrice, $currentNum, 10);
     }
