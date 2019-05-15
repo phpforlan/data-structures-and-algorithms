fa867281864c5b64fcbc49b6ecd2b5e817597ab6
lurenzhong <lurenzhong@didichuxing.com>
2019-05-15 08:36:45 +0800

    fix

--- php/buy/CalBuy.php
+++ php/buy/CalBuy.php
@@ -164,12 +164,12 @@ class CalBuy
      */
     public function test()
     {
-        $currentAveragePrice = 211.988; //当前仓位均价
-        $currentNum = 15; //当前仓位张数
+        $currentAveragePrice = 200; //当前仓位均价
+        $currentNum = 3; //当前仓位张数
         $batchNum = 5; //购买总批次
         $multiple = 10; //默认10倍 10/20
         $maxLossRate = 0.2; //能承受的最大亏损率(当亏损率达到该值时，会触发补仓操作。如果发现没有更多资金可补仓，则立刻止损，并发送止损通知)
-        $supplyLossRate = 0.12; //补仓亏损率(每次补仓完成后的仓位亏损率)
+        $supplyLossRate = 0.1; //补仓亏损率(每次补仓完成后的仓位亏损率)
         $isBtc = false; //是否是比特币
 
         $this->doHigher($currentAveragePrice, $currentNum, $batchNum, $multiple, $maxLossRate, $supplyLossRate,
